58ce9e86fca6945f9d6898c70969483edf4c223d
Ngoc Anh <ngocanhphamk2@gmail.com>
2020-12-24 13:25:58 +0700

    update 1:25 24/12/2020

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ import express from 'express'
 import dotenv from 'dotenv'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
-import ProjectStatus from './models/projectStatus.js'
 
 dotenv.config()
 const app = express()
@@ -21,14 +20,5 @@ mongoose.connect(
     () => console.log("Connected to DB")
 )
 
-app.get('/test', async (req, res) => {
-    try {
-        const newProjectStatus = new ProjectStatus(req.body)
-        const saveProjectStatus = await newProjectStatus.save()
-        return res.json(saveProjectStatus)
-    } catch (error) {
-        return res.json(error)
-    }
-})
 
 app.listen(port, () => console.log(`Server is running`))
