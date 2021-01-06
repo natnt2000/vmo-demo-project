f0aa47e3ab744b73725c083ac5837f7d949ea833
Ngoc Anh <ngocanhphamk2@gmail.com>
2021-01-06 16:52:33 +0700

    setup helmet and cors

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 import express from 'express'
-import dotenv from 'dotenv'
-import bodyParser from 'body-parser'
+
 import database from './config/database'
+
 import routes from './routes'
 
-dotenv.config()
+import { config } from 'dotenv'
+import { urlencoded, json } from 'body-parser'
+import cors from 'cors'
+import helmet from 'helmet'
+
+config()
+
 const app = express()
 const port = 3000
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(urlencoded({ extended: false }))
+app.use(json())
+app.use(cors())
+app.use(helmet())
 
 database()
 routes(app)
